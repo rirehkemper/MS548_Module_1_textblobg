2b22e880703182951fce0f5c686556a7b2c730ab
Richard Allen Rehkemper <68981248+rirehkemper@users.noreply.github.com>
2025-01-23 12:07:42 -0700

    Add files via upload

---
+++ MS548_mod_1_textBlob_Rehkemper.py
@@ -0,0 +1,12 @@
+# Name: Richard Allen Rehkemper
+# Date: 01/21/2025
+# Program Description: Menu-driven programe that allows the user to input a menu selection and then displays the appropriate output. The program will continue to run until the user selects the exit option. 
+# MS-548 Assignment 1  with Professor Tony Hinton.
+# Estimated Time: 2 hours
+
+# Time Log:
+# 01/21/2025 - Spent 30 minutes having to download the environment again on a different device.
+
+
+from textblob import TextBlob
+
